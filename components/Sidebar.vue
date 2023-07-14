c6bcb979116968e56393144771e140c30064f1b5
Valijon Coder <valijoncoder9085@gmail.com>
2023-07-14 06:20:40 -0700

    change backgroun

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,35 +1,42 @@
 <template>
   <v-app dark>
     <v-app-bar
-      fixed
       app
+      elevation="0"
+      class="px-2"
+      style="background-color: inherit"
     >
       <v-spacer/>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-toolbar-title>
+      </v-toolbar-title>
+      {{ title }}
+
       <v-spacer/>
     </v-app-bar>
-    <v-main>
+    <v-main :style="{backgroundImage: `url(${background})`}" style="background-repeat: no-repeat; background-attachment: fixed; background-position: center; background-size: cover">
       <v-container>
         <Nuxt/>
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import background from '../static/img_background.jpg'
 export default {
   name: 'SidebarPages',
   data() {
     return {
+      background,
       fixed: false,
-      title: 'Fizika'
+      img: require('../static/img_background.jpg'),
+      title: 'Fizika fanini rivojlantirishga qaratilgan elektron platforma prototipi'
     }
-  }
+  },
 }
 </script>
+<style lang="scss" scoped>
+.toolbar-text{
+  font-family: "Calibri Light";
+}
+</style>
