04c71f94472a1e749d30c91b4f5da784e4221a00
Valijon Coder <valijoncoder9085@gmail.com>
2023-07-13 23:08:17 -0700

    video dowload

--- pages/video-id/_id.vue
+++ pages/video-id/_id.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    <video v-if="1" width="400" controls>
-      <source src="" type="video/avi" >
+  <div class="d-flex align-center justify-center">
+    <video v-if="id == 1" width="50%" controls>
+      <source src="/video/1.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 2" width="50%" controls>
+      <source src="/video/2.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 3" width="50%" controls>
+      <source src="/video/3.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 4" width="50%" controls>
+      <source src="/video/4.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 5" width="50%" controls>
+      <source src="/video/5.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 6" width="50%" controls>
+      <source src="/video/6.mp4" type="video/mp4" >
+    </video>
+    <video v-else-if="id == 7" width="50%" controls>
+      <source src="/video/7.mp4" type="video/mp4" >
     </video>
   </div>
 </template>
