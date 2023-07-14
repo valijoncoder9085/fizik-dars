f16e7a264ad72b5fefc43d39f3e1bc50344dbc32
Valijon Coder <valijoncoder9085@gmail.com>
2023-07-14 08:19:51 -0700

    video img change

--- pages/video-id/_id.vue
+++ pages/video-id/_id.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="d-flex align-center justify-center">
-    <video v-if="id == 1" controls>
-      <source src="/video/1.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 2" controls>
-      <source src="/video/2.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 3" controls>
-      <source src="/video/3.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 4" controls>
-      <source src="/video/4.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 5" controls>
-      <source src="/video/5.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 6" controls>
-      <source src="/video/6.mp4" type="video/mp4">
-    </video>
-    <video v-else-if="id == 7" controls>
-      <source src="/video/7.mp4" type="video/mp4">
-    </video>
+  <div>
+    <v-card class="d-flex align-center justify-center">
+      <v-card-text>
+        <video v-if="id == 1" width="100%" controls>
+          <source src="/video/1.mp4" type="video/mp4">
+        </video>
+        <video v-else-if="id == 2" width="100%" controls>
+          <source src="/video/2.mp4" type="video/mp4">
+        </video>
+        <video v-else-if="id == 3" width="100%" controls>
+          <source src="/video/3.mp4" type="video/mp4">
+        </video>
+        <video v-else-if="id == 4" width="100%" controls>
+          <source src="/video/4.mp4" type="video/mp4">
+        </video>
+        <video v-else-if="id == 5" width="100%" controls>
+          <source src="/video/5.mp4" type="video/mp4">
+        </video>
+        <video v-else-if="id == 6" width="100%" controls>
+          <source src="/video/6.mp4" type="video/mp4">
+        </video>
+        <video class="video-div" v-else-if="id == 7" controls width="100%">
+          <source src="/video/7.mp4" type="video/mp4">
+        </video>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "VideoIdPage_id",
-  data(){
-    return{
+  data() {
+    return {
       id: '',
     }
   },
